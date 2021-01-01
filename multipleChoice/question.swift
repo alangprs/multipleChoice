bfba87aee2ab90c33d51e81ec018f4c9775981f9
will <alangprs1@gmail.com>
2021-01-02 01:12:05 +0800

    加入question

---
+++ multipleChoice/question.swift
@@ -0,0 +1,16 @@
+//
+//  question.swift
+//  practicePlace
+//
+//  Created by 翁燮羽 on 2020/12/28.
+//
+
+import Foundation
+
+struct Question:Codable{
+    var 問題:String
+    var 答案:String
+    var 選項1:String
+    var 選項2:String
+    var 選項3:String
+}
